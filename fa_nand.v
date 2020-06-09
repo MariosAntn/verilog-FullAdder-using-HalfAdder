b361470790c0e3e76a742ac702f071364440edbf
Marios Antoniou <66298083+MariosAntn@users.noreply.github.com>
2020-06-09 05:20:50 +0300

    Update fa_nand.v

--- fa_nand.v
+++ fa_nand.v
@@ -1,9 +1,9 @@
 module fa_nand(A, B, Ci, S, Co);
-input A, B, Ci;
-output S, Co;
-wire p1, p2, w1, w2, w3, w4;
-ha_nand SUM1(A, B, p1, w1), SUM2(p1, Ci, S, w2);
-nand g1(w3, w2, w2); 
-nand g2(w4, w1, w1);
-nand g3(Co, w3, w4);
+  input A, B, Ci;
+  output S, Co;
+  wire p1, p2, w1, w2, w3, w4;
+  ha_nand SUM1(A, B, p1, w1), SUM2(p1, Ci, S, w2);
+  nand g1(w3, w2, w2); 
+  nand g2(w4, w1, w1);
+  nand g3(Co, w3, w4);
 endmodule
